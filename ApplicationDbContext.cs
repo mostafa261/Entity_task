417be3ccd625414d9c494e34c378bb036cac1fce
mostafa261 <105760156+mostafa261@users.noreply.github.com>
2024-05-10 01:59:28 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .ToTable("NewTask");
+
+         //   ''''''''''''''''''''''''''
+            modelBuilder.Entity<Task>()
+            .Property(b => b.data)
+            .HasColumnName("Deadline");
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Elgohary;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string title { get; set; } = null!;
+        public string? description { get; set; }
+        public DateTime data { get; set; }
+    }
+}
